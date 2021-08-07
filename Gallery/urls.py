caf94f0b082fb03adee09e197ae1cc09c7de1d40
Aavnik✔ kumar <aav5roy@gmail.com>
2021-08-07 20:34:14 +0530

    static root cor

--- Gallery/urls.py
+++ Gallery/urls.py
@@ -24,8 +24,9 @@ urlpatterns = [
 
     path('',include('Home.urls')),
 ]
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL,
+                              document_root=settings.MEDIA_ROOT)
 
-urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += staticfiles_urlpatterns()
